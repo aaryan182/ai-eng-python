cf2815478a6d1036bb81f5fee7942d83fb5b0a48
aaryan <aaryanbajaj385@gmail.com>
2025-11-24 18:29:41 +0530

    modules, package, venv

---
+++ day-3-problem/main.py
@@ -0,0 +1,6 @@
+from my_tools.math_ops import add, multiply
+from my_tools.text_ops import to_upper, word_agent
+
+print(add(10, 20))
+print(to_upper("hello"))
+print(word_agent("python is easy"))
---
+++ day-3-problem/my_tools/text_ops.py
@@ -0,0 +1,5 @@
+def to_upper(text):
+    return text.upper()
+
+def word_agent(text):
+    return len(text.split())
